0a2897e3aee6ba4dabaa1c988fa2469000e160ff
Nikolay Popov <helioproduct.sync@gmail.com>
2023-12-16 00:35:07 +0300

    base auth added

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from . import db
 
@@ -7,14 +7,4 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "Login"
-
-
-@auth.route("/signup")
-def signup():
-    return "Signup"
-
-
-@auth.route("/logout")
-def logout():
-    return "Logout"
+    return render_template("login.html")
